353b757029d4eb2ebe94a9b64bda1495652c3904
joshkim <joshkim47@gmail.com>
2020-12-19 18:17:40 -0500

    Add IPO ws: add extract data function and recent ipo data

--- stock_market/data/ipo.py
+++ stock_market/data/ipo.py
@@ -6,21 +6,39 @@ import requests
 IPO_URL = "https://www.marketwatch.com/tools/ipo-calendar"
 
 
-class IPO:
+class IPO(object):
     # Web Scraped data
-    _data_ws = bs4.BeautifulSoup(requests.get(IPO_URL).content, "html.parser").find(
-        "div", class_="element__body j-tabPanes"
+    _data_ws = (
+        bs4.BeautifulSoup(requests.get(IPO_URL).content, "html.parser")
+        .find("div", class_="element__body j-tabPanes")
+        .find_all("table")
     )
 
     # 4 resulting ipo data sets
-    recent_ipo = None
-    upcoming_ipo = None
-    future_ipo = None
-    withdrawn_ipo = None
+    _recent_ipo = None
+    _upcoming_ipo = None
+    _future_ipo = None
+    _withdrawn_ipo = None
 
-    def setup_dataframe(self, web_table: bs4.element.Tag) -> pd.DataFrame:
+    @property
+    def recent_ipo(self):
+        # Check if this web scraping has already been run
+        if self._recent_ipo:
+            return self._recent_ipo
+        else:
+            # First index is recent_ipo
+            ws_val = self._data_ws[0]
+
+            # Extract data
+            data = self.extract_data(ws_val)
+
+            self._recent_ipo = data
+            return data
+
+    @staticmethod
+    def extract_data(web_table: bs4.element.Tag) -> pd.DataFrame:
         """
-        Initial data frame setup for each ipo data sets.
+        Web scrape table header and values as pandas dataframe.
 
         Parameters
         ----------
@@ -29,16 +47,27 @@ class IPO:
 
         Returns
         -------
-        df_setup: pd.DataFrame
-            Initial data frame, with populated table headers.
+        df: pd.DataFrame
+            Pandas dataframe with proper column and values.
 
         """
         # Ensure the headers are there at the minimum
         if len(web_table.find_all("th")) == 0:
             raise ValueError("There is no table header available for this data set.")
 
-        # Extract header information
-        columns = web_table.find_all("tr")[0].text.strip().split("\n")
-        df_setup = pd.DataFrame(columns=columns)
+        # Find all headers
+        all_info = web_table.find_all("tr")
 
-        return df_setup
+        # Extract header information
+        columns = all_info[0].text.strip().split("\n")
+        del all_info[0]
+
+        # Append values to rows in data
+        values = []
+        for i in range(len(all_info)):
+            ipo_info = all_info[i].text.strip().split("\n")
+            values.append(ipo_info)
+
+        df = pd.DataFrame(values, columns=columns)
+
+        return df
