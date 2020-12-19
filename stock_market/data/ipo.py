8b5cc224d4028d31710d53460f4e26014e05d43a
joshkim <joshkim47@gmail.com>
2020-12-19 17:04:52 -0500

    Add IPO ws: dataframe setup function

--- stock_market/data/ipo.py
+++ stock_market/data/ipo.py
@@ -1,6 +1,6 @@
 import bs4
+import pandas as pd
 import requests
-
 
 # URL with IPO information (from MarketWatch)
 IPO_URL = "https://www.marketwatch.com/tools/ipo-calendar"
@@ -17,3 +17,28 @@ class IPO:
     upcoming_ipo = None
     future_ipo = None
     withdrawn_ipo = None
+
+    def setup_dataframe(self, web_table: bs4.element.Tag) -> pd.DataFrame:
+        """
+        Initial data frame setup for each ipo data sets.
+
+        Parameters
+        ----------
+        web_table: bs4.element.Tag
+            The BeautifulSoup data containing table values.
+
+        Returns
+        -------
+        df_setup: pd.DataFrame
+            Initial data frame, with populated table headers.
+
+        """
+        # Ensure the headers are there at the minimum
+        if len(web_table.find_all("th")) == 0:
+            raise ValueError("There is no table header available for this data set.")
+
+        # Extract header information
+        columns = web_table.find_all("tr")[0].text.strip().split("\n")
+        df_setup = pd.DataFrame(columns=columns)
+
+        return df_setup
