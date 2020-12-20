f7c20e42ecdd6936df808a55d642132e001a9a4d
joshkim <joshkim47@gmail.com>
2020-12-19 19:37:31 -0500

    Add data.ipo required packages

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ setup(
     author="Josh Kim",
     author_email="joshkim47@gmail.com",
     packages=find_packages(exclude=["docs", "tests*"]),
-    install_requires=[""],
+    install_requires=[
+        "pandas>=1.1.5",
+        "pandas-datareader>=0.9.0",
+        "bs4>=0.0.1",
+        "requests>=2.25.1",
+    ],
     python_requires=">=3.6",
 )
