699b5f6f2a9cbefbcaa5b180dd5f0f4b25f19d24
joshkim <joshkim47@gmail.com>
2020-12-26 19:23:29 -0500

    Fix relative path issue

--- stock_market/data/constants.py
+++ stock_market/data/constants.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
 import pandas as pd
 
 # Stock categories (from S&P 500)
 STOCK_CATEGORY = [
-    industry.lower() for industry in list(set(pd.read_csv("index/sp500.csv").Sector))
+    industry.lower()
+    for industry in list(
+        set(pd.read_csv(Path(__file__).parent / "index/sp500.csv").Sector)
+    )
 ]
