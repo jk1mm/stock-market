a806a15d1d592e41fa162865ad83b007e4a362e1
joshkim <joshkim47@gmail.com>
2020-12-28 17:49:55 -0500

    Add within day percent change info

--- stock_market/data/stocks.py
+++ stock_market/data/stocks.py
@@ -8,7 +8,7 @@ from pandas_datareader import data
 # Reference:
 # https://towardsdatascience.com/how-to-get-market-data-from-the-nyse-in-less-than-3-lines-python-41791212709c
 def get_ticker(
-    ticker: str, start_date: str, end_date: str = None
+    ticker: str, start_date: str, end_date: str = None,
 ) -> Optional[pd.DataFrame]:
     """
     Extracts stock prices over a date period from Yahoo Finance.
@@ -72,6 +72,13 @@ def get_ticker(
 
         # Add metric
         stock_pct_change_d2d = previous_pct_change + stock_pct_change_d2d
-        stock_data["pct_change_dd"] = stock_pct_change_d2d
+        stock_data["Pct: Day Over Day"] = stock_pct_change_d2d
+
+        # 2) Add within day percent change
+        stock_data["Pct: Within Day"] = (
+            (np.array(stock_data["Close"]) - np.array(stock_data["Open"]))
+            / np.array(stock_data["Open"])
+            * 100.0
+        )
 
     return stock_data
