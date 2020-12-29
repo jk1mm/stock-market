140b8f6a325929be7ed392a5659ca0ed9d5f4716
joshkim <joshkim47@gmail.com>
2020-12-28 19:19:32 -0500

    Patch update version and update package req

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="stock_market",
-    version="1.0.0",
-    description="Modules related to stock market analysis.",
+    version="1.0.1",
+    description="Modules related to stock market model.",
     author="Josh Kim",
     author_email="joshkim47@gmail.com",
     packages=find_packages(exclude=["docs", "tests*"]),
     install_requires=[
         "pandas>=1.1.5",
+        "numpy>=1.19.4",
         "pandas-datareader>=0.9.0",
         "bs4>=0.0.1",
         "requests>=2.25.1",
