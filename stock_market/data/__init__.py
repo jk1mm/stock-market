10dc275cb612ac2415e6b6a5ffe0ee26108ea800
joshkim <joshkim47@gmail.com>
2020-12-30 15:59:49 -0500

    Add ipo and stocks module to init

--- stock_market/data/__init__.py
+++ stock_market/data/__init__.py
@@ -2,5 +2,8 @@ from pathlib import Path as _Path
 
 import pandas as _pandas
 
+from ._ipo import IPO
+from ._stocks import get_ticker
+
 # S&P data
 SP500 = _pandas.read_csv(_Path(__file__).parent / "index/sp500.csv")
