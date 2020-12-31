be30d74fd2ed3ccff70f8b7a5434faf2dd80d5b8
joshkim <joshkim47@gmail.com>
2020-12-31 16:26:58 -0500

    Add price history logic

--- stock_market/analysis/ipo.py
+++ stock_market/analysis/ipo.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from pandas_datareader._utils import RemoteDataError
 
-from stock_market.data import IPO
+from stock_market.data import IPO, get_ticker
 
 
 class RecentIPO(object):
@@ -11,6 +12,9 @@ class RecentIPO(object):
     # Get recent ipo data
     recent_ipo = IPO().recent_ipo
 
+    # Helper variables
+    _price_history = None  # Storing stock price history
+
     # Stats on different views
     _summary = None
     _individual_view = None
@@ -19,14 +23,44 @@ class RecentIPO(object):
     def summary(self) -> pd.DataFrame:
         """
         High level summary of recent IPOs.
+        1) Aggregate stock performance in one percentage.
+        2) Total price shift per stocks since IPO
+        3) Optimal sell day histogram
 
         """
         # Check if summary function has been run
-        if type(self._summary) is not None:
-            return self._summary
-        else:
+        if self._summary is None:
+            recent_ipo = self.recent_ipo
+            today = pd.to_datetime("today")
+            valid_stocks = []
+
             # Loop through each stock to see validity in US/CDN stock exchange
-            None
+            for stock_i in range(len(recent_ipo)):
+                try:
+                    ticker = recent_ipo.iloc[stock_i, :].Ticker
+                    ticker_data = get_ticker(
+                        ticker=ticker, start_date=today - pd.Timedelta(days=30)
+                    )
+                except KeyError:
+                    pass
 
+    def price_history(self):
+        """
+        Stores all recent ipo historical data.
+        """
 
-# TODO: FutureIPO
+        if self._price_history is None:
+            _price_history = {}
+            recent_ipo = self.recent_ipo
+            today = pd.to_datetime("today")
+
+            for stock_i in range(len(recent_ipo)):
+                try:
+                    ticker = recent_ipo.iloc[stock_i, :].Ticker
+                    _price_history[ticker] = get_ticker(
+                        ticker=ticker, start_date=today - pd.Timedelta(days=30)
+                    )
+                except RemoteDataError:
+                    pass
+
+            self._price_history = _price_history
