3ffafef8b0f24bc065c6d8b0c5c0522006822938
joshkim <joshkim47@gmail.com>
2021-01-05 12:16:19 -0500

    Fix index ticker symbol change and import path

--- stock_market/data/__init__.py
+++ stock_market/data/__init__.py
@@ -2,8 +2,8 @@ from pathlib import Path as _Path
 
 import pandas as _pandas
 
-from ._ipo import IPO
-from ._stocks import get_ticker
+from stock_market.data._ipo import IPO
+from stock_market.data._stocks import get_ticker
 
 # S&P data
 SP500 = _pandas.read_csv(_Path(__file__).parent / "index/sp500.csv")
