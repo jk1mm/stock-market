e5d843fb8ea7e30e57004393179dad3bb75b3a5f
joshkim <joshkim47@gmail.com>
2021-01-14 00:31:59 -0500

    Add in index analysis constants

--- stock_market/data/constants.py
+++ stock_market/data/constants.py
@@ -2,3 +2,15 @@ from stock_market.data import SP500
 
 # Stock categories (from S&P 500)
 STOCK_CATEGORY = [industry.lower() for industry in list(set(SP500.Sector))]
+
+
+# Analysis
+# --------
+
+# Index: WebScrape constants
+
+# SP500
+SP500_LINK = "https://www.marketwatch.com/investing/index/spx"
+PERFORMANCE_PERIODIC = "performance"
+PERFORMANCE_TOP_STOCKS = "ByIndexGainers"
+PERFORMERS_BOTTOM_STOCKS = "ByIndexDecliners"
