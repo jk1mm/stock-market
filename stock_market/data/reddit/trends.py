b96341d4ec11d041d7bb53f51c3626fd84fd81e5
joshkim <joshkim47@gmail.com>
2021-02-07 12:13:51 -0500

    Check subreddit existance and input changes

--- stock_market/data/reddit/trends.py
+++ stock_market/data/reddit/trends.py
@@ -1,11 +1,16 @@
 from typing import Dict, List, Union
 
-from ._connection import reddit_connection
+from prawcore import NotFound
+
+from stock_market.data.reddit._connection import reddit_connection
+
+TIME_FILTERS = ["all", "day", "hour", "month", "week", "year"]
 
 
 def get_reddit_top_posts(
     subreddit: str,
-    limit: int = 10,
+    num_post: int = 10,
+    time_period: str = "day",
 ) -> Dict[str, Union[List[int], List[str]]]:
     """
     Extracts top posts from Reddit's subreddit channel.
@@ -15,8 +20,11 @@ def get_reddit_top_posts(
     subreddit: str
         Subreddit channel name.
 
-    limit: int, default 10
+    num_post: int, default 10
         Number of top posts to extract.
+
+    time_period: str, default "day"
+        Time period of the top posts.
 
     Returns
     -------
@@ -24,11 +32,24 @@ def get_reddit_top_posts(
         Top post high level information: title and number of comments.
 
     """
+    time_period = time_period.lower()
+    if time_period not in TIME_FILTERS:
+        raise Warning(
+            f"Time period must be one of the following options: {TIME_FILTERS}"
+        )
+
     title_list = list()
     volume_list = list()
 
-    # Top posts in subreddit
-    top_agg = reddit_connection.subreddit(subreddit).top(limit=limit)
+    # Check if subreddit exists
+    try:
+        # Top posts in subreddit
+        top_agg = reddit_connection.subreddits.search_by_name(subreddit, exact=True)[
+            0
+        ].top(limit=num_post, time_filter=time_period)
+    except NotFound:
+        raise Warning("Specified subreddit does not exist.")
+
     for post in top_agg:
         title_list.append(post.title)
         volume_list.append(post.num_comments)
