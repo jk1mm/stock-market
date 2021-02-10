720b43c3ca3ff589df778180391df0a3d6ea98aa
joshkim <joshkim47@gmail.com>
2021-02-09 20:18:56 -0500

    Add env creds load module and update base connection

--- stock_market/data/reddit/_connection.py
+++ stock_market/data/reddit/_connection.py
@@ -5,10 +5,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-reddit_connection = praw.Reddit(
-    user_agent=os.getenv("REDDIT_APP_NAME", "reddit app"),
-    client_id=os.getenv("REDDIT_CLIENT_ID"),
-    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-    username=os.getenv("REDDIT_USERNAME"),
-    password=os.getenv("REDDIT_PASSWORD"),
-)
+# Load credentials to env
+def load_reddit_credentials(
+    client_id: str,
+    client_secret: str,
+    username: str,
+    password: str,
+    user_agent: str = "reddit app",
+):
+    os.environ["REDDIT_APP_NAME"] = user_agent
+    os.environ["REDDIT_CLIENT_ID"] = client_id
+    os.environ["REDDIT_CLIENT_SECRET"] = client_secret
+    os.environ["REDDIT_USERNAME"] = username
+    os.environ["REDDIT_PASSWORD"] = password
+
+    print("Credentials loaded!")
+
+
+def reddit_connection():
+
+    return praw.Reddit(
+        user_agent=os.getenv("REDDIT_APP_NAME", "reddit app"),
+        client_id=os.getenv("REDDIT_CLIENT_ID", ""),
+        client_secret=os.getenv("REDDIT_CLIENT_SECRET", ""),
+        username=os.getenv("REDDIT_USERNAME", ""),
+        password=os.getenv("REDDIT_PASSWORD", ""),
+    )
