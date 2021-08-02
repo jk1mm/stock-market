ed29efa8f6f857e85655780a269d9e66aeb529af
joshkim <joshkim47@gmail.com>
2021-08-01 17:50:36 -0700

    Patch update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="stock_market",
-    version="1.3.0",
+    version="1.3.1",
     description="Modules related to stock market model.",
     author="Josh Kim",
     author_email="joshkim47@gmail.com",
